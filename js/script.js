bc08dbc7aab6d89f2a524b09bb35320fda23b876
Carlos Chirito <cchirito@phsperu.com>
2017-04-04 19:22:52 -0500

    Portafolio finalizado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*
+
 $(window).scroll(function () {
     var $this = $(this),
         $head = $('header');
@@ -7,7 +7,7 @@ $(window).scroll(function () {
     } else {
         $head.removeClass("active");
     }
-})*/
+})
 
 $(document).ready(function() {
     function changeImgSize(){
@@ -27,19 +27,28 @@ $(document).ready(function() {
             change:changeImgSize
         },
       zoom: {
-    enabled: true, // By default it's false, so don't forget to enable it
+        enabled: true, // By default it's false, so don't forget to enable it
 
-    duration: 300, // duration of the effect, in milliseconds
-    easing: 'ease-in-out', // CSS transition easing function
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
 
-    // The "opener" function should return the element from which popup will be zoomed in
-    // and to which popup will be scaled down
-    // By defailt it looks for an image tag:
-    opener: function(openerElement) {
-      // openerElement is the element on which popup was initialized, in this case its <a> tag
-      // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-      return openerElement.is('img') ? openerElement : openerElement.find('img');
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+        // openerElement is the element on which popup was initialized, in this case its <a> tag
+        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
     }
-  }
+});
+    $("header nav a, a.arrow").on('click', function(e) {
+        e.preventDefault();
+
+        var section = $(this).attr("href");
+        $(section).animatescroll({
+            padding: 51
+        });
     });
+    
 });
